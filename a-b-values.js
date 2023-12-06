d93afcbdcd392cf855c38de294e4a8978240c2fb
Vasile Andruscenco <134550335+wellington-100@users.noreply.github.com>
2023-12-06 15:55:10 +0200

    Add files via upload

---
+++ a-b-values.js
@@ -0,0 +1,14 @@
+
+var a = parseFloat(prompt("Enter \"A\" value"));
+var b = parseFloat(prompt("Enter \"B\" value"));
+alert(
+    "###### VALUES ######" + 
+    "\na = " + a +
+    "\nb = " + b + 
+    "\n#### OPERATIONS ####" +
+    "\na * b = " + (a * b) +
+    "\na / b = " + (a / b) +
+    "\na + b = " + (a + b) +
+    "\na - b = " + (a - b) +
+    "\n####################"
+);
